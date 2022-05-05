02e6fd282364720e0d605c9ec458efbd9a3cc3cc
Sagar-Bhalsing <77963069+Sagar-Bhalsing@users.noreply.github.com>
2022-05-05 18:06:03 +0530

    Add files via upload

---
+++ Practical/PrimitiveDataTypes.java
@@ -0,0 +1,16 @@
+
+public class PrimitiveDataTypes {
+	public static void main(String[] args) {
+		int a = 100000;
+		char x1='A';
+		short myShort = 6000;
+		double myDouble1 = 34.254;
+		byte myByte1 = -122;
+		float myFloat1=1000.666f;
+		long x = 100000L;
+		boolean myBool = true;
+		System.out.println("Integer:"+a + "\n"+"char:"+x1 +"\n"+ "short:"+myShort +"\n"+ "double:"+myDouble1 +"\n"+ "byte:"+myByte1 +"\n"+ "Float:"+myFloat1 +"\n"+ "Long:"+x +"\n"+ "Boolean:"+myBool);
+	
+		
+	}
+}
