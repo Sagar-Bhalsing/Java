07530c41eddbb0e23f111d59904180ceb96b66b5
Sagar-Bhalsing <77963069+Sagar-Bhalsing@users.noreply.github.com>
2022-05-05 18:08:29 +0530

    Add files via upload

---
+++ Practical/CharDataType.java
@@ -0,0 +1,11 @@
+public class CharDataType{
+	
+public static void main(String[] args) {
+	char x1,x2;
+	x1='8';
+	x2='A';
+	System.out.println("First Numaber:"+x1);
+	System.out.println("Second Numaber:"+x2);
+}
+	
+}
