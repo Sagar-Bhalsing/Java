20961c8bbc8946fda15d2d24f5d411f2dbf546b8
Sagar-Bhalsing <77963069+Sagar-Bhalsing@users.noreply.github.com>
2022-07-01 01:51:54 +0530

    Add files via upload

---
+++ Daily-Activity/This/ThisWithConstructor.java
@@ -0,0 +1,28 @@
+package executor;
+
+//Write a program to test this keyword with constructor by calling default from parameterized
+
+public class ThisWithConstructor {
+
+ int a;
+
+ // Default Constructor
+ ThisWithConstructor(){
+     System.out.println("\nDefault body...");
+ }
+
+ // Parameterized Constructor
+ ThisWithConstructor(int a){
+
+     // Calling Default Constructor
+     this();
+
+     this.a = a;
+     System.out.println("Parameterized body...: " + a);
+ }
+
+ public static void main(String[] args){
+
+     ThisWithConstructor t = new ThisWithConstructor(10);
+ }
+}
