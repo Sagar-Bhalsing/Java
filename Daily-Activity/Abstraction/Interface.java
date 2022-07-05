4ad02d1440a1a20aa5d296c41438d2bb1646fcd1
Sagar-Bhalsing <77963069+Sagar-Bhalsing@users.noreply.github.com>
2022-07-06 01:08:08 +0530

    Update Interface.java

--- Daily-Activity/Abstraction/Interface.java
+++ Daily-Activity/Abstraction/Interface.java
@@ -1,21 +1,24 @@
 //Program to implement Abstraction using Interface
+//Interface is used to Implement 100% Abstraction i.e
+//Every method is abstract method 
+//Abstract methods are methods with no body
 package executor;
-interface Myinterface{
-	void disp();
-	void show();
+interface Myinterface{//Interface 
+	void disp();//Abstract method
+	void show();//Abstract method
 }
-class My implements Myinterface{
-	public void disp(){
+class My implements Myinterface{//child class of interface 
+	public void disp(){//Abstract method of interface is been declared here
 		System.out.println("Methof defined....");	
 		}
-   public void show() {
+   public void show() {//Abstract method of interface is been declared here
 	System.out.println("Method defined....");
 }
 }
 public class Interface {
 	public static void main(String[] args) {
-		Myinterface m=new My();
-		m.disp();
-		m.show();
+		Myinterface m=new My();//objection creation for interfaces is given like "interfacename obj=new childclass"
+		m.disp();//Method
+		m.show();//Method
 	}
 }
